378d14d8213e7661c7034b9518c230ee00e9a5bb
Naitais <enzo.f9849@gmail.com>
2022-08-24 15:22:05 -0300

    added hover effect

--- jsScriptFile/javaScript.js
+++ jsScriptFile/javaScript.js
@@ -1,19 +1,23 @@
-
-
-
-
-const createDiv =() =>{
-    const newDiv = document.createElement("div");
+const createDiv =(newDiv) =>{
+    newDiv = document.createElement("div");
     mainContainer.append(newDiv);
     newDiv.classList.add("gridDiv");
-    newDiv.textContent="new div content";
 }
 
 for(let r =0;r<16;r++){
     for(let i= 0; i<16; i++){
-        createDiv()
-    }
+            createDiv()
+    }  
 }
-    
+
+const hoverFunc= ()=>{
+    newDiv=addEventListener("mouseover", (e)=>{
+    e.target.classList.add("hovered");
+    })
+    newDiv = addEventListener("mousedown", (e)=>{
+    e.target.classList.remove("hovered");
+    })
+}
+hoverFunc()
 
 
