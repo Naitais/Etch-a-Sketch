773e3d9e1bf96ab2eb8984ca8702231ecfe2c861
Naitais <enzo.f9849@gmail.com>
2022-08-24 13:46:57 -0300

    created function for div created for loop to create 16x16 grid

--- jsScriptFile/javaScript.js
+++ jsScriptFile/javaScript.js
@@ -1,1 +1,19 @@
-console.log("lol");
+
+
+
+
+const createDiv =() =>{
+    const newDiv = document.createElement("div");
+    mainContainer.append(newDiv);
+    newDiv.classList.add("gridDiv");
+    newDiv.textContent="new div content";
+}
+
+for(let r =0;r<16;r++){
+    for(let i= 0; i<16; i++){
+        createDiv()
+    }
+}
+    
+
+
